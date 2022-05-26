0aae2636df6e6440bb5444a155ea5becb210be9d
Cedrus-P <80530649+Cedrus-P@users.noreply.github.com>
2022-05-26 22:18:02 +0800

    use model

--- src/components/VmodelTest.vue
+++ src/components/VmodelTest.vue
@@ -1,16 +1,33 @@
 <script>
-  export default{
-    props:{
-      modelValue:{
-        type: Number,
-        default: 0
-      }
-    }
-  }
+import { h } from 'vue'
+export default {
+	render() {
+		const emit = this.$emit
+		const onclick = this.onclick
+		return h('div', [
+			h(
+				'div',
+				{
+					onClick() {
+						emit('update:modelValue', 'new value')
+					}
+				},
+				`i am comp, ${this.modelValue}`
+			),
+			h(
+				'button',
+				{
+					onClick() {
+						onclick()
+					}
+				},
+				'buty it!'
+			)
+		])
+	}
+}
 </script>
 <template>
-  <div @click="$emit('update:modelValue', modelValue +1)">{{counter}}</div>
+	<div @click="$emit('update:modelValue', modelValue + 1)">{{ counter }}</div>
 </template>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
