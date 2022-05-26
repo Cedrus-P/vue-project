242f9e24e3b5e96b52b4befc4b476e33336f009a
Cedrus-P <80530649+Cedrus-P@users.noreply.github.com>
2022-05-26 19:52:03 +0800

    add new test

--- src/components/VmodelTest.vue
+++ src/components/VmodelTest.vue
@@ -1,8 +1,15 @@
-<script setup lang="ts">
- 
+<script>
+  export default{
+    props:{
+      modelValue:{
+        type: Number,
+        default: 0
+      }
+    }
+  }
 </script>
 <template>
-  <div class="demo-container"></div>
+  <div @click="$emit('update:modelValue', modelValue +1)">{{counter}}</div>
 </template>
 <style lang="scss" scoped>
 
